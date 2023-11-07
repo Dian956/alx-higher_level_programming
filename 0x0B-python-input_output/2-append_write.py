65d617dcd7efad22c1e867d62e2f1e67a0f8255d
Dian956 <dianamwelu554@gmail.com>
2023-11-07 11:30:49 -0800

    input output

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
-def read_lines(filename="", nb_lines=0):
-    """function that reads n lines of a text file and prints it to stdout"""
+append_write = __import__('2-append_write').append_write
 
-    lines = 0
-    with open(filename) as f:
-        for lin in f:
-            lines += 1
-        f.seek(0)
-        if nb_lines <= 0 or nb_lines >= lines:
-            print(f.read(), end="")
-        else:
-            for i in range(0, nb_lines):
-                print(f.readline(), end="")
+nb_characters_added = append_write("file_append.txt", "This School is so cool!\n")
+print(nb_characters_added)

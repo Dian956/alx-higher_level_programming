c611b097ed93c510517442bf474b5a93bb4d433d
Dian956 <dianamwelu554@gmail.com>
2024-01-29 00:14:03 +0300

    net1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-""" script that fetches https://alx-intranet.hbtn.io/status """
+""" Script that fetches https://alx-intranet.hbtn.io/status """
 import urllib.request
+import urllib.error
 
 if __name__ == "__main__":
         url = "https://alx-intranet.hbtn.io/status"
-            req = urllib.request.Request(url)
+            try:
+                        req = urllib.request.Request(url)
+                                with urllib.request.urlopen(req) as response:
+                                                html = response.read()
+                                                            print("Body response:")
+                                                                        print("\t- type: {}".format(type(html)))
+                                                                                    print("\t- content: {}".format(html))
+                                                                                                print("\t- utf8 content: {}".format(html.decode('utf-8')))
+                                                                                                    except urllib.error.URLError as e:
+                                                                                                                print("Error fetching URL:", e)
+                                                                                                                    except urllib.error.HTTPError as e:
+                                                                                                                                print("HTTP Error:", e)
 
-                with urllib.request.urlopen(req) as response:
-                            html = response.read()
-                                    print("Body response:")
-                                            print("\t- type: {}".format(type(html)))
-                                                    print("\t- content: {}".format(html))
-                                                            print("\t- utf8 content: {}".format(html.decode('utf-8')))

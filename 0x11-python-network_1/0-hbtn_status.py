fd93f1ec16f90b393b2704621411d584d2a669ae
Dian956 <dianamwelu554@gmail.com>
2024-01-30 16:17:46 +0300

    NET1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-"""A script that
-- fetches https://alx-intranet.hbtn.io/status.
-- uses urlib package
 """
+A script that:
+    - fetches https://alx-intranet.hbtn.io/status.
+    - uses urllib package.
+    """
 
+    import urllib.request
+    import urllib.error
 
-if __name__ == '__main__':
-        import urllib.request
-
-            with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-                        content = res.read()
-                                print("Body response:")
-                                        print("\t- type: {}".format(type(content)))
-                                                print("\t- content: {}".format(content))
-                                                        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    if __name__ == '__main__':
+            try:
+                        with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+                                        content = res.read()
+                                                    print("Body response:")
+                                                                print("\t- type: {}".format(type(content)))
+                                                                            print("\t- content: {}".format(content))
+                                                                                        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+                                                                                            except urllib.error.URLError as e:
+                                                                                                        print("URLError:", e)
+                                                                                                            except urllib.error.HTTPError as e:
+                                                                                                                        print("HTTPErroor:", e)

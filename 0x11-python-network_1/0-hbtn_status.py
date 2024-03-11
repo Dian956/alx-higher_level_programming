410a19e8e2886826b717450f45874ff9f67f42b8
Dian956 <dianamwelu554@gmail.com>
2024-03-11 05:16:41 -0700

    net1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,22 +1,13 @@
 #!/usr/bin/python3
-"""
-A script that:
-    - fetches https://alx-intranet.hbtn.io/status.
-    - uses urllib package.
-    """
+"""Fetches https://intranet.hbtn.io/status."""
+import urllib.request
 
-    import urllib.request
-    import urllib.error
 
-    if __name__ == '__main__':
-            try:
-                        with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-                                        content = res.read()
-                                                    print("Body response:")
-                                                                print("\t- type: {}".format(type(content)))
-                                                                            print("\t- content: {}".format(content))
-                                                                                        print("\t- utf8 content: {}".format(content.decode('utf-8')))
-                                                                                            except urllib.error.URLError as e:
-                                                                                                        print("URLError:", e)
-                                                                                                            except urllib.error.HTTPError as e:
-                                                                                                                        print("HTTPErroor:", e)
+if __name__ == "__main__":
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
